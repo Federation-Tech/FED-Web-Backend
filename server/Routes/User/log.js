12c4965ea497f23d4c903eea09d2c0eba5e81fc0
achyut vardhan <achyutvardhan234@gmail.com>
2023-03-22 12:24:18 +0530

    session

--- server/Routes/User/log.js
+++ server/Routes/User/log.js
@@ -4,36 +4,32 @@ const jwt  = require('jsonwebtoken')
 const bcrypt =  require('bcryptjs')
 const mongoose =  require('mongoose')
 const User = require('../../models/user.model')
-
+const cookieParser = require('cookie-parser');
+const sessions = require('express-session');
 require('dotenv').config()
 
 mongoose.connect('mongodb://localhost:27017/fed-user');  
+const oneDay = 1000 * 60 * 60 * 24;
 
-//register user
-// router.post('/user/register/',async(req,res)=>{
-//     try {
-//         const newPass =  await bcrypt.hash(req.body.user.password,10 )
-//         const result =  await User.create({
-//             userName :  req.body.user.store,
-//             userpass : newPass
-//         });
-//         res.json({status : 'ok'})
-//     } catch (error) {
-//         res.json({status : 'error', error : 'Duplicate store'})
-//     }
-// });
+//session middleware
+router.use(sessions({
+    secret: process.env.session_key ,
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
 
-
+//cookie middleware
+router.use(cookieParser());
 
 //login user
-router.get('/user/login' , async(req,res)=>{
+router.post('/' , async(req,res)=>{
     const result = await  User.findOne({
         userName : req.body.email,
     });
     
     if (!result) {return res.json({status : 'error', user : false })};
-    const isPasswordValid =  await bcrypt.compare(req.body.password,result.userpass);
-    if (isPasswordValid) {
+    if (req.body.password===result.userpass) {
         const token =  jwt.sign({
             userName : result.userName,
         }, process.env.USER_SECRET_KEY)
@@ -42,3 +38,5 @@ router.get('/user/login' , async(req,res)=>{
         res.json({status : 'error' , user : false})
     }
 });
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ const cors =  require('cors')
 const mongoose =  require('mongoose')
 app.use(cors())
 app.use(express.json())
+// app.use(express.urlencoded({ extended: false }));
 
-
-
+app.use('/user/login', require('./Routes/User/log'))
 app.get('*', (req,res)=>{
     console.log("error 404")
 })
