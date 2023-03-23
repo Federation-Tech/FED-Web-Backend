86255fbb48097e94db49fb542fd7ca0e3ff96d5f
achyut vardhan <achyutvardhan234@gmail.com>
2023-03-23 10:57:17 +0530

    session creeated

--- server/Routes/User/log.js
+++ server/Routes/User/log.js
@@ -4,23 +4,30 @@ const jwt  = require('jsonwebtoken')
 const bcrypt =  require('bcryptjs')
 const mongoose =  require('mongoose')
 const User = require('../../models/user.model')
-const cookieParser = require('cookie-parser');
-const sessions = require('express-session');
+// const cookieParser = require('cookie-parser');
+// const sessions = require('express-session');
 require('dotenv').config()
 
 mongoose.connect('mongodb://localhost:27017/fed-user');  
 const oneDay = 1000 * 60 * 60 * 24;
 
 //session middleware
-router.use(sessions({
-    secret: process.env.session_key ,
-    saveUninitialized:true,
-    cookie: { maxAge: oneDay },
-    resave: false
-}));
+// router.use(sessions({
+//     secret: process.env.session_key ,
+//     saveUninitialized:true,
+//     cookie: { maxAge: oneDay },
+//     resave: false
+// }));
 
-//cookie middleware
-router.use(cookieParser());
+// //cookie middleware
+// router.use(cookieParser());
+
+
+// let session;
+
+
+//already logged in
+
 
 //login user
 router.post('/' , async(req,res)=>{
@@ -29,14 +36,23 @@ router.post('/' , async(req,res)=>{
     });
     
     if (!result) {return res.json({status : 'error', user : false })};
-    if (req.body.password===result.userpass) {
+    // req.body.password===result.userpass
+    if (  req.body.password===result.userpass) {
         const token =  jwt.sign({
             userName : result.userName,
-        }, process.env.USER_SECRET_KEY)
+        }, process.env.USER_SECRET_KEY,{ expiresIn: '86400s' })
+
+
+        // session = req.session;
+        // session.userid = token;
+        // console.log(req.session)
         res.json({status :'ok', user : token})
     } else{
         res.json({status : 'error' , user : false})
     }
 });
-
+router.post('/logout',(req,res)=>{
+    req.session.destroy();
+    res.redirect('/');
+})
 module.exports = router
