957d15dad84718757f2d12799c164100e1e5c23a
achyut vardhan <achyutvardhan234@gmail.com>
2023-03-27 18:24:49 +0530

    login complete

---
+++ controllers/user.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken')
+const bcrypt =  require('bcryptjs')
+const mongoose =  require('mongoose')
+const User  =  require ('../models/user.model')
+require("dotenv").config();
+
+const login = async(req,res)=>{
+    const result  = await User.findOne({
+        email : req.body.username
+    })
+    const result2 =  await User.findOne({
+        mobno : req.body.username
+    })
+    if(!result || !result2){return res.json({status : "error", message : "invalid credential"}) }
+    if(req.body.password === result.password && req.body.password === result2.password)
+    {
+        if(result.isvalid == true && result2.isvalid == true)
+        {
+            const token  =  jwt.sign({
+                userName : result.userName,
+            }, process.env.USER_SECRET_KEY,{ expiresIn: '86400s' })
+            res.json({status : "ok" , user : token})
+        }else{
+           return  res.redirect('/verification')
+        }
+    }else{
+        res.json({status : "error" , user : false})
+    }
+}
+
+exports.login = login;
--- models/user.model.js
+++ models/user.model.js
@@ -1,8 +1,18 @@
 const mongoose =  require('mongoose')
 
 const User =  new mongoose.Schema({
-    userName:{type : String , required : true , unique : true },
-    userpass : { type : String , required : true }
+    userid:{type : String , required : true , unique : true },
+    password : { type : String , required : true },
+    email : { type : String , required : true , unique : true },
+    name : { type : String , required : true },
+    rollno : { type : String , required : true , unique : true },
+    mobno : { type : Number , required : true },
+    mobno2 : { type : Number , required : true },
+    source : { type : String , required : true },
+    uniname : { type : String , required : true },
+    access : { type : String , required : true },
+    extradata : { type : String , required : true },
+    isvalid : { type : Boolean , required : true },
 },
 {
     collection : 'user-Log-data'
