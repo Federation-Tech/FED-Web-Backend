daf44f155af166ac5eb3aa685f4dd007f140877b
achyut vardhan <achyutvardhan234@gmail.com>
2023-03-28 21:07:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,18 @@ app.use(express.json());
 
 // app.use(express.urlencoded({ extended: false }));
 mongoose.connect('mongodb://localhost:27017/fed-user') 
+<<<<<<< Updated upstream
 app.use("/user", require("./Routes/User/router"));
+=======
+app.use('/user', require('./Routes/User/router'))
+
+app.get('/test',(req,res)=>{
+    console.log("test page");
+})
+app.get('*', (req,res)=>{
+    console.log("error 404")
+})
+>>>>>>> Stashed changes
 
 app.get("*", (req, res) => {
   console.log("error 404");
