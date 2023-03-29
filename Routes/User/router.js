7d88c5ae3fce892c96f42ab46fbefd090bc320c5
vinit agarwal <vinitagarwal.garg@gmail.com>
2023-03-30 01:35:09 +0530

    final routes

--- Routes/User/router.js
+++ Routes/User/router.js
@@ -1,8 +1,10 @@
-const express =  require('express')
-const router =  express.Router();
-const mongoose =  require('mongoose')
-const controller =  require('../../controllers/user')
-
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const controller = require("../../controllers/user");
+const { postData } = require("../../../controllers/registrationController");
+//registration
+router.post("/user/register", postData);
 //login user
-router.post('/login' , controller.login);
-module.exports = router
+router.post("/login", controller.login);
+module.exports = router;
