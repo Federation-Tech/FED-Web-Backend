a175afcac1d911c6dd39a51ce26449b409a9fd06
Rishav Singh <rishavrajcrj@gmail.com>
2023-03-30 10:26:40 +0530

    Update router.js

--- Routes/User/router.js
+++ Routes/User/router.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const controller = require("../../controllers/user");
 const registrationController = require("../../controllers/registrationController");
+
 //registration
 router.post("/register", registrationController.register);
+
 //login user
 router.post("/login", controller.login);
 module.exports = router;
