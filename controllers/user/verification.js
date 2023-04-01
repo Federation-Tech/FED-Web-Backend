fcefc5232e16b972ab6ffe8612c086e9683e5de3
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-02 00:17:06 +0530

    Update verification.js

--- controllers/user/verification.js
+++ controllers/user/verification.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const db = require("../../models/user-model");
 const mailer = require("../../mailer/mailer");
+
 async function verfication(req, res) {
   var token = req.params.token;
   var vemail = await jwt.verify(token, process.env.verification_token_key);
@@ -9,6 +10,7 @@ async function verfication(req, res) {
     "<h1 style='test-align:center'>You Are Verified Please visit <a href ='https://www.google.com'>Login Page</a> to login to your account</h1>"
   );
 }
+
 async function sendverficationmail(email, name) {
   var mail = {
     to: email,
@@ -20,6 +22,7 @@ async function sendverficationmail(email, name) {
   };
   console.log(await mailer.sendMail(mail));
 }
+
 function validate(req, res, next) {
   if (!req) next();
   jwt.verify(req.body.token, process.env.access_token_key, (err, user) => {
