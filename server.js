7c9c6770163e08e60472cb447ecd66dfb2a6a5be
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-02 00:15:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const mongoose = require("mongoose");
 const verfication = require("./controllers/user/verification");
-app.use(cors());
+
+const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 // app.use(express.urlencoded({ extended: false }));
 mongoose.connect("mongodb://localhost:27017/fed-user");
+
 app.use("/auth", require("./Routes/User/router"));
 app.use("/validatetest", verfication.validate, (req, res) => {
   res.send(req.body.user);
