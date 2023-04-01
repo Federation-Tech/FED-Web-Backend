ce419ea4d0ec77a3cffb6db7ca5ea0457e21fd8e
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-02 00:15:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
 const cors = require("cors");
+const express = require("express");
 const mongoose = require("mongoose");
 const verfication = require("./controllers/user/verification");
 
