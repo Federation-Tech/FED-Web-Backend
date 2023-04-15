57ac0547c93c318567dba3637a954d135156398e
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-15 12:42:57 +0530

    Update router.js

--- Routes/profile/router.js
+++ Routes/profile/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const profilecontroller = require("../../controllers/profile/getdata");
 const middleware = require("../../middleware/validator");
+
 //registration
 router.post("/getprofile", middleware.validate, profilecontroller.getdata);
 
