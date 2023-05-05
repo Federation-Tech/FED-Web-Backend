f8a2db2f74610a9f910f1979db14600c3bc4b807
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-05 20:50:28 +0530

    Update contact-us.js

--- models/contact-us.js
+++ models/contact-us.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const contactData = new Schema({
   date: { type: String, required: true },
-  email: { type: String, required: true},
+  email: { type: String, required: true },
   name: { type: String, required: true },
   message: { type: String, required: true },
 });
