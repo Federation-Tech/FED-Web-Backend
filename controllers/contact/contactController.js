21fef01ebba7728a16e11bc70b5754de160a2981
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-05 21:01:59 +0530

    Create contactController.js

---
+++ controllers/contact/contactController.js
@@ -0,0 +1,21 @@
+var moment = require("moment");
+
+// models
+const contactData = require("../../models/contact-us");
+
+async function postcontact(req, res, next) {
+  try {
+    const { name, email, message } = req.body;
+    var date = moment().utc("Asia/Kolkata").format("DD-MM-yyyy").toString();
+    var contactus = new contactData({ date, name, email, message });
+    await contactus.save();
+    res.sendStatus(200);
+  } catch {
+    res.sendStatus(400);
+  }
+}
+
+const getContact = async (req, res, next) => {};
+
+exports.postcontact = postcontact;
+exports.getContact = getContact;
