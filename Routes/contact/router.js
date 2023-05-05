61de26e065f7b826615bc911cb627318b8ed00b5
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-05 21:10:13 +0530

    Update router.js

--- Routes/contact/router.js
+++ Routes/contact/router.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
-
+const { check } = require("express-validator");
 // Controller
 const contactController = require("../../controllers/contact/contactController");
 
 // Public || Post || Contact Us
-router.post("/postContact", contactController.postcontact);
+router.post(
+  "/postContact",
+  [
+    check("name", "name is Required").not().isEmpty(),
+    check("email", "email is Required").not().isEmpty(),
+    check("message", "message is Required").not().isEmpty(),
+  ],
+  contactController.postcontact
+);
 
 // Private (Admin) || Get || Contact Us
 router.get("/getContact", contactController.getContact);
