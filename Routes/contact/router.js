c58e8e406a9a0bbfc0de29bf76c097ff3b7dd2c8
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-05 20:55:56 +0530

    Update router.js

--- Routes/contact/router.js
+++ Routes/contact/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const postContact = require("../../controllers/contact/postContact");
 
+// Public || Post || Contact Us
 router.post("/postcontact", postContact);
 
 module.exports = router;
