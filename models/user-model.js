c5992229f43403cd12cc88df30ca84e3a23ddfeb
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-05 23:42:43 +0530

    Update user-model.js

--- models/user-model.js
+++ models/user-model.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const registrationData = new Schema({
-  password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
   name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   access: { type: String, required: true },
   extradata: { type: Array, required: true }, // array of objects
   isvalid: { type: Boolean, required: true },
+  password: { type: String, required: true },
   img: { type: String, required: true },
 });
 
