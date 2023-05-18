1db5c5f0d22bcfa9df430df2ec41f12aafcc8d8a
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-18 20:28:28 +0530

    Update member.js

--- Routes/member/member.js
+++ Routes/member/member.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
+const memberController = require("./../../controllers/members/membersController");
 
 // Public || Get All Mebers Data
 router.get("/", memberController.showMembers);
@@ -8,7 +9,13 @@ router.get("/", memberController.showMembers);
 // Private || Add Member
 router.post(
   "/addMember",
-  //   [check("name", "name is Required").not().isEmpty()],
+  [
+    check("name", "name is Required").not().isEmpty(),
+    check("access", "access is Required").not().isEmpty(),
+    check("email", "email is Required").not().isEmpty(),
+    check("img", "img is Required").not().isEmpty(),
+  ],
+
   memberController.addMembers
 );
 
