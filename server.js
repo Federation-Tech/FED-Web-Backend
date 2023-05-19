c12a636436e90638584e63edb706b676585fbaec
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-19 23:13:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,12 +11,27 @@ connectDB();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://127.0.0.1:5173",
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: "http://127.0.0.1:5173",
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE,OPTIONS"
+  );
+
+  next();
+});
 
 app.use(cookieParser());
 
