fea964fbe70093bdbe321f4575331e2e7bb048b3
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-20 18:32:50 +0530

    Update verification.js

--- controllers/user/verification.js
+++ controllers/user/verification.js
@@ -19,13 +19,39 @@ async function sendverficationmail(email, name) {
   var mail = {
     to: email,
     subject: "Verify Your Mail Please",
-    text:
-      process.env.server_address +
-      "/auth/verification/" +
-      ~jwt.sign(email, process.env.verification_token_key),
+    html: `<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Email Verification</title>
+    </head>
+    <body>
+        <h1>Dear User,</h1>
+        <p>Thank you for creating an account with FED KIIT. We are excited to have you as a valued supporter of our community. To ensure the security of your account and provide you with the best experience, we kindly request your cooperation in verifying your email address.</p>
+        <p>To complete the verification process, please click on the link below:</p>
+        <p><a href='${
+          process.env.server_address +
+          "/auth/verification/" +
+          jwt.sign(email, process.env.verification_token_key)
+        }'></a>Verify Email Address</p>
+        <p>Thank you for your cooperation. We look forward to serving you and providing you with a seamless experience on our platform.</p>
+        <p>Connect with us now on:</p>
+        <ul>
+            <li><a href="https://instagram.com/fedkiit?igshid=MzRlODBiNWFlZA==">Instagram</a></li>
+            <li><a href="https://www.linkedin.com/company/fedkiit/">LinkedIn</a></li>
+            <li><a href="https://youtube.com/@federationkiit">YouTube</a></li>
+        </ul>
+        <p>Best regards,</p>
+        <p>Team FED</p>
+    </body>
+    </html>`,
   };
   console.log(await mailer.sendMail(mail));
 }
 
 exports.verify = verfication;
 exports.mail = sendverficationmail;
+
+// process.env.server_address +
+//   "/auth/verification/" +
+//   ~jwt.sign(email, process.env.verification_token_key);
