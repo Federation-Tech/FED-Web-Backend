e643d574994c03fc2bc42e42263d79b6051cf6d9
muskan-bajaj <muskanvijaybajaj@gmail.com>
2023-05-28 13:55:34 +0530

    access code

--- controllers/members/membersController.js
+++ controllers/members/membersController.js
@@ -1,24 +1,66 @@
 const { validationResult } = require("express-validator");
+const passGenerator = require("generate-password");
+const bcrypt = require("bcrypt");
+const mailer = require("./../../mailer/mailer");
+const User = require("../../models/user-model");
 
 const showMembers = async (req, res) => {
+  console.log("request to show all member");
   res.status(202).json("people");
 };
 
 const addMembers = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+    // console.log("error");
     return res.status(400).json({ errors: errors.array() });
   }
 
   const { name, access, email, img } = req.body;
 
   try {
-    res
-      .status(202)
-      .json({ name, email, access, img, isvalid: false, password: 123 });
+    console.log("sending data to db");
+    const password = passGenerator.generate({
+      length: 10,
+      numbers: true,
+      symbols: true,
+    });
+    const hashedPassword = bcrypt.hashSync(
+      password,
+      "$2b$10$Q0RPeouqYdTToq76zoccIO"
+    );
+    // console.log(password);
+    // console.log(hashedPassword);
+    console.log("Password Sent");
+    mailer.sendMail({
+      to: email,
+      subject: "Member Login Details",
+      text: `Dear ${name},
+            
+            Your login details for the FED website are as follows:
+              Email: ${email}
+              Password: ${password}`,
+    });
+    console.log("Mail sent");
+    const data = new User({
+      email: email,
+      name: name,
+      access: access,
+      isvalid: true,
+      password: hashedPassword,
+      img: img,
+      extradata: "",
+      RollNumber: "",
+      School: "",
+      College: "",
+      MobileNo: "",
+      selected: "",
+    });
+    console.log("db updated");
+    return res.status(200).json({ status: "ok" });
   } catch (err) {
     console.log(err);
-    res.status(505).json({ msg: "error", err });
+    res.status(400).json({ msg: "error", err });
   }
 };
 
