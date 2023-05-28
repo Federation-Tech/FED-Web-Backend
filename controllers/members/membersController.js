77c5bd910839f26b4b827e26be2efd57331c430f
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-28 15:18:03 +0530

    Update membersController.js

--- controllers/members/membersController.js
+++ controllers/members/membersController.js
@@ -23,54 +23,60 @@ const addMembers = async (req, res) => {
     console.log("Member already exists");
     return res.status(400).json({ code: 1, message: "Member already exists" });
   }
-  try {
-    console.log("sending data to db");
-    const password = passGenerator.generate({
-      length: 10,
-      numbers: true,
-      symbols: true,
-    });
-    const hashedPassword = bcrypt.hashSync(
-      password,
-      "$2b$10$Q0RPeouqYdTToq76zoccIO"
-    );
 
-    console.log("Password Sent");
+  if (email.includes("@")) {
+    try {
+      console.log("sending data to db");
+      const password = passGenerator.generate({
+        length: 10,
+        numbers: true,
+        symbols: true,
+      });
+      const hashedPassword = bcrypt.hashSync(
+        password,
+        "$2b$10$Q0RPeouqYdTToq76zoccIO"
+      );
 
-    mailer.sendMail({
-      to: email,
-      subject: "Member Login Details",
-      text: `Dear ${name},
+      console.log("Password Sent");
+
+      mailer.sendMail({
+        to: email,
+        subject: "Member Login Details",
+        text: `Dear ${name},
             
             Your login details for the FED website are as follows:
               Email: ${email}
               Password: ${password}`,
-    });
+      });
 
-    console.log("Mail sent");
+      console.log("Mail sent");
 
-    const data = new User({
-      email: email,
-      name: name,
-      access: access,
-      isvalid: true,
-      password: hashedPassword,
-      img: img,
-      extradata: {},
-      RollNumber: "None",
-      School: "None",
-      College: "None",
-      MobileNo: "None",
-      selected: "None",
-    });
+      const data = new User({
+        email: email,
+        name: name,
+        access: access,
+        isvalid: true,
+        password: hashedPassword,
+        img: img,
+        extradata: {},
+        RollNumber: "None",
+        School: "None",
+        College: "None",
+        MobileNo: "None",
+        selected: "None",
+      });
 
-    await data.save();
-    console.log("+ Member Added");
+      await data.save();
+      console.log("+ Member Added");
 
-    return res.status(200).json({ status: "ok" });
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({ msg: "error", err });
+      return res.status(200).json({ status: "ok" });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ msg: "error", err });
+    }
+  } else {
+    console.log("invalid data entered sending err...");
+    res.status(400).json({ code: 2, error: "invalid details entered" });
   }
 };
 
