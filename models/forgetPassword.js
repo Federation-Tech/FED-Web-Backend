0586cbf3d01f42a8cb3b2373fb4baa347bcb4716
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-28 20:49:40 +0530

    Update forgetPassword.js

--- models/forgetPassword.js
+++ models/forgetPassword.js
@@ -2,8 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const forgetPassword = new Schema({
-  email: { type: String, required: true, unique: true },
-  otp: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("forgotPassword", forgetPassword);
