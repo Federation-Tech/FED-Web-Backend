7a0883d66a804fca5f4f1fa3fdb82e2ce252da2c
BinaySaahu <100080926+BinaySaahu@users.noreply.github.com>
2023-05-29 21:25:12 +0530

    google_signup

---
+++ controllers/user/googleRegistration.js
@@ -0,0 +1,79 @@
+const registrationSchema = require("../../models/user-model");
+const gravatar = require("gravatar");
+const User = require("../../models/user-model");
+
+//
+const postData = async (req, res) => {
+  console.log(`Registration request received for ${req.body.email}`);
+
+  req.body.isvalid = true;
+//   req.body.img = gravatar.url(
+//     req.body.email,
+//     { s: "200", r: "pg", d: "mm" },
+//     true
+//   );
+
+  if (req.body.email === "fedkiit@gmail.com") {
+    req.body.access = 0;
+  } else {
+    req.body.access = 1;
+  }
+
+  const {
+    email,
+    password,
+    name,
+    access,
+    extradata,
+    isvalid,
+    RollNumber,
+    School,
+    College,
+    MobileNo,
+    selected,
+    img,
+  } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if ((req.body.extradata = "" || !req.body.extradata)) {
+    req.body.extradata = {};
+  }
+
+  if (user) {
+    console.log("User already exists");
+    return res.status(400).json({ code: 1, message: "User already exists" });
+  }
+  if (email.includes("@")) {
+    try {
+      const data = new registrationSchema({
+        email,
+        password,
+        name,
+        access,
+        extradata,
+        isvalid,
+        img,
+        RollNumber,
+        School,
+        College,
+        MobileNo,
+        selected,
+      });
+
+      await data.save();
+      
+      console.log("registration done");
+
+      return res.status(200).json({ status: "ok" });
+    } catch (err) {
+      console.log("registration err " + err);
+      return res.status(400).json({ code: 2, error: err.message });
+    }
+  } else {
+    console.log("invalid data entered sending err...");
+    res.status(400).json({ code: 2, error: "invalid details entered" });
+  }
+};
+
+exports.register = postData;
