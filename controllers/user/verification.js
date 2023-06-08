72a07d165515a4ed479a728b48570af75145ea0d
me-mystic <Shrutikolla.2002@gmail.com>
2023-06-08 12:41:37 +0530

    .

--- controllers/user/verification.js
+++ controllers/user/verification.js
@@ -14,8 +14,8 @@ async function verfication(req, res) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <!-- <link rel="stylesheet" href="css/index.css"> -->
+        <link rel="icon" href=""FedLogo.png"" />
+        <title>Federation of Entrepreneurship Development</title>
         <style>
             @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap");
     
