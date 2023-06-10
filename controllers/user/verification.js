9188961842462c44d9412661ac4e4144052bf67f
me-mystic <Shrutikolla.2002@gmail.com>
2023-06-10 12:06:43 +0530

    .

--- controllers/user/verification.js
+++ controllers/user/verification.js
@@ -14,7 +14,7 @@ async function verfication(req, res) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" href=""FedLogo.png"" />
+        <link rel="icon" href="https://uploads-ssl.webflow.com/629d87f593841156e4e0d9a4/62eeaa9927e6aea4ff13590e_FedLogo.png" />
         <title>Federation of Entrepreneurship Development</title>
         <style>
             @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap");
