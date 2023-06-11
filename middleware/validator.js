03f185e13eabc0eaba20ae781533779a2806b43b
vinit agarwal <vinitagarwal.garg@gmail.com>
2023-06-11 23:52:46 +0530

    validator update

--- middleware/validator.js
+++ middleware/validator.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 function validate(req, res, next) {
   console.log("token is ",req.headers.auth_token)
   if (!req) next();
-  jwt.verify(req.headers.auth_token , process.env.access_token_key, (err, user) => {
+  jwt.verify(req.headers.authorization , process.env.access_token_key, (err, user) => {
     if (err) {
       console.log("verification failed")
       return res.status(403).json({ code: 4 });
