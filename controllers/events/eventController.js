9543fd7880a5e8387c0cbabe418f64796d1ae4ab
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-06-12 11:16:59 +0530

    .

--- controllers/events/eventController.js
+++ controllers/events/eventController.js
@@ -2,40 +2,49 @@ const User = require("../../models/user-model");
 const { validationResult } = require("express-validator");
 const db = require("./../../models/event");
 
+const parseDate = (dateString) => {
+  const dateParts = dateString.split(".");
+  const day = parseInt(dateParts[0]);
+  const month = parseInt(dateParts[1]) - 1; // Months are zero-based (0-11)
+  const year = parseInt(dateParts[2]);
+  return new Date(year, month, day);
+};
 //add event
 const addEvent = async (req, res) => {
   try {
     const { title, date, image, description } = req.body;
-    const user = req.body.access;
-    console.log(user);
+
     //validation
-    if (req.body.access == 1) {
-      if (!title) {
-        return res.send({ error: "title is require" });
-      }
-      if (!date) {
-        return res.send({ error: "date is require" });
-      }
-      if (!image) {
-        return res.send({ error: "image is require" });
-      }
-      if (!description) {
-        return res.send({ error: "description is require" });
-      }
-      const newevent = await new db({
-        title,
-        date,
-        image,
-        description,
-      }).save();
-      res.status(201).send({
-        success: true,
-        message: "event created successfully",
-        newevent,
-      });
-    } else {
-      console.log("Access denied");
+
+    if (!title) {
+      return res.send({ error: "title is require" });
     }
+    if (!date) {
+      return res.send({ error: "date is require" });
+    }
+    if (!image) {
+      return res.send({ error: "image is require" });
+    }
+    if (!description) {
+      return res.send({ error: "description is require" });
+    }
+    // // Parse and convert the date string
+    // const dateParts = date.split(".");
+    // const day = parseInt(dateParts[0]);
+    // const month = parseInt(dateParts[1]) - 1; // Months are zero-based (0-11)
+    // const year = 2000 + parseInt(dateParts[2]); // Assuming years are in the format 'YY'
+    // const parsedDate = new Date(year, month, day);
+    const newevent = await new db({
+      title,
+      date: parseDate(date),
+      image,
+      description,
+    }).save();
+    res.status(201).send({
+      success: true,
+      message: "event created successfully",
+      newevent,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
@@ -50,39 +59,36 @@ const addEvent = async (req, res) => {
 const editEvent = async (req, res) => {
   try {
     const eventId = req.params.id;
-    if (res.body.access == 0) {
-      let getevent;
-      try {
-        getevent = await db.findById(eventId);
-      } catch (err) {
-        console.log(err);
-        return res
-          .status(500)
-          .json({ success: false, message: "Error finding event" });
-      }
-      let title, date, image, description;
-      if (getevent) {
-        ({ title, date, image, description } = req.body);
-      } else {
-        return res
-          .status(404)
-          .json({ success: false, message: "Event with this id not found" });
-      }
-      getevent.title = title;
-      getevent.date = date;
-      getevent.image = image;
-      getevent.description = description;
-      try {
-        await getevent.save();
-        res.status(200).json({ success: true });
-      } catch (err) {
-        console.log(err);
-        res
-          .status(500)
-          .json({ success: false, message: "Error saving updated event" });
-      }
+
+    let getevent;
+    try {
+      getevent = await db.findById(eventId);
+    } catch (err) {
+      console.log(err);
+      return res
+        .status(500)
+        .json({ success: false, message: "Error finding event" });
+    }
+    let title, date, image, description;
+    if (getevent) {
+      ({ title, date, image, description } = req.body);
     } else {
-      console.log("Access denied");
+      return res
+        .status(404)
+        .json({ success: false, message: "Event with this id not found" });
+    }
+    getevent.title = title;
+    getevent.date = parseDate(date);
+    getevent.image = image;
+    getevent.description = description;
+    try {
+      await getevent.save();
+      res.status(200).json({ success: true });
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ success: false, message: "Error saving updated event" });
     }
   } catch (e) {
     res.status(500).json({ msg: "error" });
@@ -113,15 +119,12 @@ const getEvent = async (req, res) => {
 const deleteEvent = async (req, res) => {
   try {
     const { id } = req.params;
-    if (res.body.access == 0) {
-      await db.findByIdAndDelete(id);
-      res.status(200).send({
-        success: true,
-        message: "event deleted successfully",
-      });
-    } else {
-      console.log("Access denied");
-    }
+
+    await db.findByIdAndDelete(id);
+    res.status(200).send({
+      success: true,
+      message: "event deleted successfully",
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
