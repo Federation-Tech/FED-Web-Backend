889f502c52ade9c392d5d9cb4ffd5b236f719f51
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-21 12:59:35 +0530

    Update router.js

--- Routes/event/router.js
+++ Routes/event/router.js
@@ -2,14 +2,19 @@ const express = require("express");
 const router = express.Router();
 const eventController = require("./../../controllers/events/eventController");
 const middleware = require("../../middleware/validator");
+
 //get event
 router.get("/getevent", eventController.getEvent);
+
 //validation
 router.use(middleware);
+
 //add event
 router.post("/addevent", eventController.addEvent);
+
 //edit event
 router.patch("/editevent/:id", eventController.editEvent);
+
 //delete event
 router.delete(
   "/deleteevent/:id",
