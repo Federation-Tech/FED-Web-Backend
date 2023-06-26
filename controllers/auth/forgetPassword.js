efa3ab6a514e66b947e1b34272422eb9453fb96c
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-26 13:41:14 +0530

    Update forgetPassword.js

--- controllers/auth/forgetPassword.js
+++ controllers/auth/forgetPassword.js
@@ -15,11 +15,7 @@ const sendotp = async (req, res) => {
   }
   try {
     const otp = Math.floor(100000 + Math.random() * 900000);
-    // const data = new db({
-    //   email,
-    //   otp,
-    // });
-    // await data.save();
+
     const email = result.email;
 
     await db.updateOne(
@@ -77,7 +73,6 @@ const resetpassword = async (req, res) => {
   const result = await User.findOne({
     email: req.body.email,
   });
-  
 
   if (!result) {
     return res.status(401).json({ message: "Email doesn't exist" });
