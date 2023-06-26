f8e701a48a4e1d204456ec657057a4f813557f62
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-26 13:41:22 +0530

    Update updateProfile.js

--- controllers/auth/updateProfile.js
+++ controllers/auth/updateProfile.js
@@ -1,11 +1,11 @@
 const userSchema = require("../../models/user-model");
 
-//
 const updateData = async (req, res) => {
   console.log(`Updation request received for ${req.body.email}`);
   try {
     const { email, name, RollNumber, School, College, MobileNo, selected } =
       req.body;
+
     if (email.includes("@") && email.includes(".")) {
       const filter = { email: email };
       const options = { upsert: true };
@@ -20,10 +20,12 @@ const updateData = async (req, res) => {
           selected: selected,
         },
       };
+
       const rslt = await userSchema.updateOne(filter, updateDoc, options);
       console.log(rslt);
-      const response = await userSchema.findOne(filter,{_id:0,__v:0});
-      return res.status(200).json({ message: "ok",response});
+
+      const response = await userSchema.findOne(filter, { _id: 0, __v: 0 });
+      return res.status(200).json({ message: "ok", response });
     } else {
       return res.status(400).json({ code: 3 });
     }
