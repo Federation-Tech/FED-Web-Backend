dd314a4e49bf6203dec66964b616b69d1c78bee9
vinit agarwal <vinitagarwal.garg@gmail.com>
2023-06-26 21:40:21 +0530

    file cleaning

--- Routes/profile/router.js
+++ Routes/profile/router.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const profilecontroller = require("../../controllers/profile/getdata");
+const updateProfile = require("../../controllers/auth/updateProfile");
 
 //registration
 router.get("/getprofile", profilecontroller.getdata);
+
+//update profile
+router.post("/updateProfile", updateProfile.updateData);
 
 module.exports = router;
