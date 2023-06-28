248603808f6d873b8d7338bb89575cb42136769f
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-28 17:11:59 +0530

    Update googleAuth.js

--- controllers/auth/googleAuth.js
+++ controllers/auth/googleAuth.js
@@ -1,80 +1,6 @@
 const jwt = require("jsonwebtoken");
-const gravatar = require("gravatar");
 const User = require("../../models/user-model");
 const { validationResult } = require("express-validator");
-
-const GoogleReg = async (req, res) => {
-  console.log(`Registration request received for ${req.body.email}`);
-
-  req.body.isvalid = true;
-  req.body.img = gravatar.url(
-    req.body.email,
-    { s: "200", r: "pg", d: "mm" },
-    true
-  );
-
-  if (req.body.email === "fedkiit@gmail.com") {
-    req.body.access = 0;
-  } else {
-    req.body.access = 1;
-  }
-
-  const {
-    email,
-    password,
-    name,
-    access,
-    extradata,
-    isvalid,
-    RollNumber,
-    School,
-    College,
-    MobileNo,
-    selected,
-    img,
-  } = req.body;
-
-  const user = await User.findOne({ email });
-
-  if ((req.body.extradata = "" || !req.body.extradata)) {
-    req.body.extradata = {};
-  }
-
-  if (user) {
-    console.log("User already exists");
-    return res.status(400).json({ code: 1, message: "User already exists" });
-  }
-  if (email.includes("@")) {
-    try {
-      const data = new registrationSchema({
-        email,
-        password,
-        name,
-        access,
-        extradata,
-        isvalid,
-        img,
-        RollNumber,
-        School,
-        College,
-        MobileNo,
-        selected,
-      });
-
-      await data.save();
-
-      console.log("registration done");
-
-      return res.status(200).json({ status: "ok" });
-    } catch (err) {
-      console.log("registration err " + err);
-      return res.status(400).json({ code: 2, error: err.message });
-    }
-  } else {
-    console.log("invalid data entered sending err...");
-    res.status(400).json({ code: 2, error: "invalid details entered" });
-  }
-};
 
 const googleSignUp = async (req, res) => {
   const errors = validationResult(req);
@@ -111,8 +37,6 @@ const googleSignUp = async (req, res) => {
         return res.json({ status: false, message: "verfication error" });
       }
     } else {
-      console.log("------------------------------------- New User");
-
       console.log(
         `FED-TECH -> User does not exists Requested by ${email} [Google Login] `
       );
@@ -123,5 +47,4 @@ const googleSignUp = async (req, res) => {
   }
 };
 
-exports.GoogleReg = GoogleReg;
 exports.googleSignUp = googleSignUp;
