645987db59b58320c0f48af1b38d14b065b82471
muskan-bajaj <muskanvijaybajaj@gmail.com>
2023-07-07 15:55:35 +0530

    team

--- models/user-model.js
+++ models/user-model.js
@@ -2,63 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const registrationData = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  access: {
-    type: String,
-    required: true,
-  },
-  extradata: {
-    type: Array,
-    required: true,
-  },
-  isvalid: {
-    type: Boolean,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  img: {
-    type: String,
-    required: true,
-  },
-  RollNumber: {
-    type: String,
-    required: true,
-  },
-  School: {
-    type: String,
-    required: true,
-  },
-  College: {
-    type: String,
-    required: true,
-  },
-  MobileNo: {
-    type: String,
-    required: true,
-  },
-  selected: {
-    type: String,
-    required: true,
-  },
-  hashID: {
-    type: String,
-    default: "00HLk~",
-  },
-});
-
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  access: { type: String, required: true },
+  extradata: { type: Array, required: true }, // array of objects
+  isvalid: { type: Boolean, required: true },
+  password: { type: String, required: true },
+  img: { type: String, required: true },
+  RollNumber: { type: String, required: true },
+  School: { type: String, required: true },
+  College: { type: String, required: true },
+  MobileNo: { type: String, required: true },
+  selected: { type: String, required: true },
+  linkedin:{type:String,default:"NA"},
+  github:{type:String,default:"NA"},
+  regForm:[{type:Schema.Types.ObjectId,ref:'form',unique:true}]
+})
 module.exports = mongoose.model("userdb", registrationData);
