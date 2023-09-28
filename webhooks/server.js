d3091aa8a1cd76385f7777fe5816485973ce4283
vinit agarwal <vinitagarwal.garg@gmail.com>
2023-09-28 23:58:59 +0530

    maza

--- webhooks/server.js
+++ webhooks/server.js
@@ -1,40 +1,18 @@
 const express = require("express");
-const crypto = require('crypto')
-const bodyParser = require('body-parser')
 const app = express();
 const secret = "vinit";
 
-// For these headers, a sigHashAlg of sha1 must be used instead of sha256
-// GitHub: X-Hub-Signature
-// Gogs:   X-Gogs-Signature
-const sigHeaderName = 'x-hub-signature-256'
-const sigHashAlg = 'sha256'
 var githubMiddleware = require('github-webhook-middleware')({
-  secret: "vinit"});
+  secret: secret});
+
 app.post("/push",githubMiddleware, async(req,res)=>{
-    console.log(req.headers)
-    console.log(req.body)
+    if(req.body.repository.name == "FED-Web-Backend"){
+      console.log((req.body.ref).split("/")[2])
+    }
     res.status(202).send("ok")
 });
 
+
 app.listen(7000, async () => {
-  console.log(`FED-TECH -> Server is running on Port`);
+  console.log(`FED-TECH -> Githook Server is running on Port 7000`);
 });
-
-
-
-function verifyPostData(req, res) {
-    
-    if (!req.body) {
-      return console.log('Request body empty')
-    }
-  
-    const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8')
-    const hmac = crypto.createHmac(sigHashAlg, secret)
-    const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.body).digest('hex'), 'utf8')
-    if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
-      return console.log(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`)
-    }
-  
-    console.log("check")
-  }
