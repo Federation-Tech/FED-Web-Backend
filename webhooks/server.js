860806ae6410d01c23242b6de14dc593880ba514
vinit agarwal <vinitagarwal.garg@gmail.com>
2023-09-29 00:26:43 +0530

    formating

--- webhooks/server.js
+++ webhooks/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
 const secret = "vinit";
-const mainDir = "/root/FED-Web-Backend"
-const mainBranch = "main"
-const betaDir = "/root/beta/FED-Web-Backend"
-const betaBranch = "main"
-
+const mainDir = "/root/FED-Web-Backend";
+const mainBranch = "main";
+const betaDir = "/root/beta/FED-Web-Backend";
+const betaBranch = "main";
 var githubMiddleware = require("github-webhook-middleware")({
   secret: secret,
 });
@@ -14,22 +13,28 @@ app.post("/push", githubMiddleware, async (req, res) => {
   if (req.body.repository.name == "FED-Web-Backend") {
     var branch = req.body.ref.split("/")[2];
     if (branch == mainBranch) {
-      var yourscript = exec(`sh gitpull.sh "${mainDir}" ${mainBranch}`, (error, stdout, stderr) => {
-        console.log(stdout);
-        console.log(stderr);
-        if (error !== null) {
-          console.log(`exec error: ${error}`);
+      var yourscript = exec(
+        `sh gitpull.sh "${mainDir}" ${mainBranch}`,
+        (error, stdout, stderr) => {
+          console.log(stdout);
+          console.log(stderr);
+          if (error !== null) {
+            console.log(`exec error: ${error}`);
+          }
         }
-      });
+      );
     }
     if (branch == betaBranch) {
-      var yourscript = exec(`sh gitpull.sh "${betaDir}" ${betaBranch}`, (error, stdout, stderr) => {
-        console.log(stdout);
-        console.log(stderr);
-        if (error !== null) {
-          console.log(`exec error: ${error}`);
+      var yourscript = exec(
+        `sh gitpull.sh "${betaDir}" ${betaBranch}`,
+        (error, stdout, stderr) => {
+          console.log(stdout);
+          console.log(stderr);
+          if (error !== null) {
+            console.log(`exec error: ${error}`);
+          }
         }
-      });
+      );
     }
   }
   res.status(202).send("ok");
