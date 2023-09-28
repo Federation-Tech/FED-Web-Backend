7e4563db88ba494a6468e8109b0aa6a213ca0e63
vinit agarwal <vinitagarwal.garg@gmail.com>
2023-09-28 06:26:26 +0530

    maza

--- webhooks/server.js
+++ webhooks/server.js
@@ -2,56 +2,35 @@ const express = require("express");
 const app = express();
 const crypto = require("crypto")
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-app.use("/push", async(req,res)=>{
+const sigHeaderName = 'X-Hub-Signature-256'
+const sigHashAlg = 'sha256'
+const secret = "vinit";
+app.use(bodyParser.json({
+    verify: (req, res, buf, encoding) => {
+      if (buf && buf.length) {
+        req.rawBody = buf.toString(encoding || 'utf8');
+      }
+    },
+  }))
+app.use("/push",verifyPostData, async(req,res)=>{
     res.status(202).send("ok")
-    console.log(await verifySignature("vinit",req.headers['x-hub-signature-256'],req.body.payload))
 });
 
 app.listen(7000, async () => {
   console.log(`FED-TECH -> Server is running on Port`);
 });
 
-let encoder = new TextEncoder();
-
-async function verifySignature(secret, header, payload) {
-    let sigHex = header;
-
-    let algorithm = { name: "HMAC", hash: { name: 'SHA-256' } };
-
-    let keyBytes = encoder.encode(secret);
-    let extractable = false;
-    let key = await crypto.subtle.importKey(
-        "raw",
-        keyBytes,
-        algorithm,
-        extractable,
-        [ "sign", "verify" ],
-    );
-
-    let sigBytes = hexToBytes(sigHex);
-    let dataBytes = encoder.encode(payload);
-    let equal = await crypto.subtle.verify(
-        algorithm.name,
-        key,
-        sigBytes,
-        dataBytes,
-    );
-
-    return equal;
-}
-
-function hexToBytes(hex) {
-    let len = hex.length / 2;
-    let bytes = new Uint8Array(len);
-
-    let index = 0;
-    for (let i = 0; i < hex.length; i += 2) {
-        let c = hex.slice(i, i + 2);
-        let b = parseInt(c, 16);
-        bytes[index] = b;
-        index += 1;
+function verifyPostData(req, res, next) {
+    if (!req.rawBody) {
+      return next('Request body empty')
     }
-
-    return bytes;
-}
+  
+    const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8')
+    const hmac = crypto.createHmac(sigHashAlg, secret)
+    const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8')
+    if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
+      return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`)
+    }
+  
+    return next()
+  }
