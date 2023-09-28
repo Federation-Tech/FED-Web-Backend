75c06b10cd90e53a0f56ebfc05a8845f2d141d2d
vinit agarwal <vinitagarwal.garg@gmail.com>
2023-09-29 00:17:10 +0530

    maza

--- webhooks/server.js
+++ webhooks/server.js
@@ -4,7 +4,7 @@ const secret = "vinit";
 const mainDir = "/root/FED-Web-Backend"
 const mainBranch = "main"
 const betaDir = "/root/beta/FED-Web-Backend"
-const betaBranch = "beta"
+const betaBranch = "main"
 
 var githubMiddleware = require("github-webhook-middleware")({
   secret: secret,
@@ -14,7 +14,16 @@ app.post("/push", githubMiddleware, async (req, res) => {
   if (req.body.repository.name == "FED-Web-Backend") {
     var branch = req.body.ref.split("/")[2];
     if (branch == mainBranch) {
-      var yourscript = exec(`sh gitpull.sh "${mainDir} ${mainBranch}"`, (error, stdout, stderr) => {
+      var yourscript = exec(`sh gitpull.sh "${mainDir}" ${mainBranch}`, (error, stdout, stderr) => {
+        console.log(stdout);
+        console.log(stderr);
+        if (error !== null) {
+          console.log(`exec error: ${error}`);
+        }
+      });
+    }
+    if (branch == betaBranch) {
+      var yourscript = exec(`sh gitpull.sh "${betaDir}" ${betaBranch}`, (error, stdout, stderr) => {
         console.log(stdout);
         console.log(stderr);
         if (error !== null) {
