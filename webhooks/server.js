7db23fbd79945cfeecf08ce5f102b23953b19c58
vinit agarwal <vinitagarwal.garg@gmail.com>
2023-09-28 05:54:33 +0530

    maza

--- webhooks/server.js
+++ webhooks/server.js
@@ -1,12 +1,55 @@
 const express = require("express");
 const app = express();
 app.use("/push", (req,res)=>{
-    console.log(req.body)
     console.log(req.headers)
+    console.log(verifySignature("123",req.headers,req.body))
 });
 
 app.listen(7000, async () => {
   console.log(`FED-TECH -> Server is running on Port`);
 });
 
+let encoder = new TextEncoder();
 
+async function verifySignature(secret, header, payload) {
+    let parts = header.split("=");
+    let sigHex = parts[1];
+
+    let algorithm = { name: "HMAC", hash: { name: 'SHA-256' } };
+
+    let keyBytes = encoder.encode(secret);
+    let extractable = false;
+    let key = await crypto.subtle.importKey(
+        "raw",
+        keyBytes,
+        algorithm,
+        extractable,
+        [ "sign", "verify" ],
+    );
+
+    let sigBytes = hexToBytes(sigHex);
+    let dataBytes = encoder.encode(payload);
+    let equal = await crypto.subtle.verify(
+        algorithm.name,
+        key,
+        sigBytes,
+        dataBytes,
+    );
+
+    return equal;
+}
+
+function hexToBytes(hex) {
+    let len = hex.length / 2;
+    let bytes = new Uint8Array(len);
+
+    let index = 0;
+    for (let i = 0; i < hex.length; i += 2) {
+        let c = hex.slice(i, i + 2);
+        let b = parseInt(c, 16);
+        bytes[index] = b;
+        index += 1;
+    }
+
+    return bytes;
+}
