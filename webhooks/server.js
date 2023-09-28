476e73713f4d09af807af0292c878caa5e430b0d
vinit agarwal <vinitagarwal.garg@gmail.com>
2023-09-28 06:20:44 +0530

    maza

--- webhooks/server.js
+++ webhooks/server.js
@@ -5,19 +5,53 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use("/push", (req,res)=>{
     res.status(202).send("ok")
-    console.log(verify_signature(req))
+    console.log(verifySignature("vinit",req.headers['x-hub-signature-256'],req.body.payload))
 });
 
 app.listen(7000, async () => {
   console.log(`FED-TECH -> Server is running on Port`);
 });
 
-const verify_signature = (req) => {
-    const signature = crypto
-      .createHmac("sha256", "vinit")
-      .update(JSON.stringify(req.body.payload))
-      .digest("hex");
-    let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
-    let untrusted =  Buffer.from(req.headers["x-hub-signature-256"], 'ascii');
-    return crypto.timingSafeEqual(trusted, untrusted);
-  };
+let encoder = new TextEncoder();
+
+async function verifySignature(secret, header, payload) {
+    let sigHex = header;
+
+    let algorithm = { name: "HMAC", hash: { name: 'SHA-256' } };
+
+    let keyBytes = encoder.encode(secret);
+    let extractable = false;
+    let key = await crypto.subtle.importKey(
+        "raw",
+        keyBytes,
+        algorithm,
+        extractable,
+        [ "sign", "verify" ],
+    );
+
+    let sigBytes = hexToBytes(sigHex);
+    let dataBytes = encoder.encode(payload);
+    let equal = await crypto.subtle.verify(
+        algorithm.name,
+        key,
+        sigBytes,
+        dataBytes,
+    );
+
+    return equal;
+}
+
+function hexToBytes(hex) {
+    let len = hex.length / 2;
+    let bytes = new Uint8Array(len);
+
+    let index = 0;
+    for (let i = 0; i < hex.length; i += 2) {
+        let c = hex.slice(i, i + 2);
+        let b = parseInt(c, 16);
+        bytes[index] = b;
+        index += 1;
+    }
+
+    return bytes;
+}
