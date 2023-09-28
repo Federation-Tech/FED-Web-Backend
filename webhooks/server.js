fa1502c6bb6bd0d9cb9308558587821b48d9b5c7
vinit agarwal <vinitagarwal.garg@gmail.com>
2023-09-29 00:03:04 +0530

    maza

---
+++ webhooks/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const secret = "vinit";
+
+var githubMiddleware = require('github-webhook-middleware')({
+  secret: secret});
+
+app.post("/push",githubMiddleware, async(req,res)=>{
+    if(req.body.repository.name == "FED-Web-Backend"){
+      console.log((req.body.ref).split("/")[2])
+    }
+    res.status(202).send("ok")
+});
+//test
+
+app.listen(7000, async () => {
+  console.log(`FED-TECH -> Githook Server is running on Port 7000`);
+});
