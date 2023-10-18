9f969fb7ac55c25daab81066cff82eb69f52693b
vinit agarwal <vinitagarwal.garg@gmail.com>
2023-10-19 03:55:12 +0530

    awai

--- webhooks/server.js
+++ webhooks/server.js
@@ -8,6 +8,7 @@ const betaDir = "/root/beta/FED-Web-Backend";
 const betaBranch = "beta";
 var cp = require('child_process');
 
+
 var githubMiddleware = require("github-webhook-middleware")({
   secret: secret,
 });
