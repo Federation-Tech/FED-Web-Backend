6f76939f3aa3c77eb12a85a4fe759ea185b0066c
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-21 08:34:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.use((req, res, next) => {
   console.log(log);
   next();
 });
+
 // Routes
 app.use("/auth", require("./Routes/auth/router"));
 app.use("/profile", validater, require("./Routes/profile/router"));
