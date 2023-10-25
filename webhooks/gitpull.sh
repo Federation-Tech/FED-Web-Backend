adeb2817b9d2e8319b608d3ee756b301036b338b
FED-Tech <128247576+fed-tech@users.noreply.github.com>
2023-10-25 16:57:58 +0530

    Beta (#26)
    beta merger

--- webhooks/gitpull.sh
+++ webhooks/gitpull.sh
@@ -1,3 +1,6 @@
+echo $1 $2
 cd $1
 git checkout $2
 git pull origin $2
+npm i
+pm2 restart $2
--- webhooks/server.js
+++ webhooks/server.js
@@ -1,42 +1,53 @@
 const express = require("express");
 const app = express();
 const secret = "vinit";
-
-const shDir = "/root/FED-Web-Backend/webhooks/gitpull.sh"
+const shDir = "/root/beta/FED-Web-Backend/webhooks/gitpull.sh"
 const mainDir = "/root/FED-Web-Backend";
 const mainBranch = "main";
 const betaDir = "/root/beta/FED-Web-Backend";
-const betaBranch = "main";
+const betaBranch = "beta";
+var cp = require('child_process');
+
 var githubMiddleware = require("github-webhook-middleware")({
   secret: secret,
 });
 const { exec } = require("child_process");
 app.post("/push", githubMiddleware, async (req, res) => {
+  console.log("Branch Called ",req.body.ref.split("/")[2])
   if (req.body.repository.name == "FED-Web-Backend") {
     var branch = req.body.ref.split("/")[2];
     if (branch == mainBranch) {
-      var yourscript = exec(
-        `sh ${shDir} "${mainDir}" ${mainBranch}`,
-        (error, stdout, stderr) => {
-          console.log(stdout);
-          console.log(stderr);
-          if (error !== null) {
-            console.log(`exec error: ${error}`);
-          }
-        }
-      );
+      var temp = cp.spawn(`sh`,[shDir,mainDir,mainBranch]);
+      temp.stdout.on('data',(data)=>{
+        console.log("out:",data.toString())
+      })
+      // var yourscript = exec(
+      //   `sh ${shDir} "${mainDir}" ${mainBranch}`,
+      //   (error, stdout, stderr) => {
+      //     console.log(stdout);
+      //     console.log(stderr);
+      //     if (error !== null) {
+      //       console.log(`exec error: ${error}`);
+      //     }
+      //   }
+      // );
     }
+
     if (branch == betaBranch) {
-      var yourscript = exec(
-        `sh ${shDir} "${betaDir}" ${betaBranch}`,
-        (error, stdout, stderr) => {
-          console.log(stdout);
-          console.log(stderr);
-          if (error !== null) {
-            console.log(`exec error: ${error}`);
-          }
-        }
-      );
+      // var yourscript = exec(
+      //   `sh ${shDir} "${betaDir}" ${betaBranch}`,
+      //   (error, stdout, stderr) => {
+      //     console.log(stdout);
+      //     console.log(stderr);
+      //     if (error !== null) {
+      //       console.log(`exec error: ${error}`);
+      //     }
+      //   }
+      // );
+      var temp = cp.spawn(`sh`,[shDir,betaDir,betaBranch]);
+      temp.stdout.on('data',(data)=>{
+        console.log("out:",data.toString())
+      })
     }
   }
   res.status(202).send("ok");
